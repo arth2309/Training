a928f6b84a62c683b12e0ad47aa6e69b3ba05e56
Arth Gandhi <PCE105@server7.com>
2024-02-02 18:34:50 +0530

    add html pager mvc day1

---
+++ mvc/HalloDoc/Controllers/PatientController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace HalloDoc.Controllers
+{
+    public class PatientController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult PatientLogin()
+        {
+            return View();
+        }
+        public IActionResult SubmitRequest()
+        {
+            return View();
+        }
+        public IActionResult PatientForgotPassword()
+        {
+            return View();
+        }
+    }
+}
+
